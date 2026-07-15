83bcd0a1723b81c7a823c74341eff7f3974ef993
imaninaitore <imaninaitore1726@gmail.com>
2026-07-15 10:51:45 +0300

    feat: import navbar and header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    </>
+
+
+  )
+}
+
+export default App
