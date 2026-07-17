2ac206ed15a23453d3afec2651e91d6ab6750872
imaninaitore <imaninaitore1726@gmail.com>
2026-07-17 23:35:29 +0300

    add footer to mainlayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 function MainLayout() {
   return (
@@ -8,6 +9,7 @@ function MainLayout() {
       <main className="min-h-screen">
         <Outlet />
       </main>
+      <Footer />
 
     </>
   );
