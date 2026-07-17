288db30d391463d54db6d3118ce05e4ef3f408ee
imaninaitore <imaninaitore1726@gmail.com>
2026-07-17 17:26:03 +0300

    create programmes file then import from context

---
+++ src/components/Progammes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useProgrammes } from '@/context/ProgrammeContext'
+function Progammes() {
+  return (
+    <div>Progammes</div>
+  )
+}
+
+export default Progammes
