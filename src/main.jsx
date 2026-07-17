adb31df4a5a0d37f3a0805210447aaa3b3575c00
imaninaitore <imaninaitore1726@gmail.com>
2026-07-17 23:43:06 +0300

    feat: wrap app in the provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './context/AuthContext'
 import { ProgrammeProvider } from './context/ProgrammeContext'
+import { ApplicationProvider } from './context/ApplicationContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <AuthProvider>
       <ProgrammeProvider>
+        <ApplicationProvider>
          <App />
+         </ApplicationProvider>
       </ProgrammeProvider>  
     </AuthProvider>
     </BrowserRouter>
