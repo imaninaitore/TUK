61625e4e80f740a57be7c25ef2a889c1cef4443b
imaninaitore <imaninaitore1726@gmail.com>
2026-07-17 10:36:30 +0300

    feat: wrap app with programme provider

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <AuthProvider>
-       <App /> 
+      <ProgrammeProvider>
+         <App />
+      </ProgrammeProvider>  
     </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
