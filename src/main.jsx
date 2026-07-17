971dd9be3cc4c6ec52d2b12741e2fb522b69ee77
imaninaitore <imaninaitore1726@gmail.com>
2026-07-17 17:40:47 +0300

    feat: import programme provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './context/AuthContext'
+import { ProgrammeProvider } from './context/ProgrammeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
